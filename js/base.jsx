a696acb927a81a5a0726b682a81ca8cf9eaa5667
Mateusz Uzdowski <mateusz@silverstripe.com>
2015-09-29 09:35:15 +1300

    BUG Properly check if the holder exists.

--- js/base.jsx
+++ js/base.jsx
@@ -4,10 +4,13 @@ var Tools = require('./tools.jsx');
 var data = Tools.readInlineData('environmentconfig-variables-model');
 var blacklist = Tools.readInlineData('environmentconfig-variables-blacklist');
 
-React.render(
-	<Variables
-		model={data}
-		blacklist={blacklist}
-		context={environmentConfigContext} />,
-	document.getElementById('environmentconfig-variables-holder')
-);
+var holder = document.getElementById('environmentconfig-variables-holder');
+if (holder) {
+	React.render(
+		<Variables
+			model={data}
+			blacklist={blacklist}
+			context={environmentConfigContext} />,
+		holder
+	);
+}
